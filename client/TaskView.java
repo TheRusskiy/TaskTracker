367bfa923e5718b32ac4c7d8d48968ee09c5133d
Dmitry Ishkov <dimanrussian@gmail.com>
2012-11-10 20:15:34 +0400

    Initial commit and files

---
+++ client/TaskView.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: TheRusskiy
+ * Date: 10.11.12
+ * Time: 19:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class TaskView {
+}
---
+++ server/TaskServer.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: TheRusskiy
+ * Date: 10.11.12
+ * Time: 19:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class TaskServer {
+}
