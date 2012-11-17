e9e47eb1806d2560b21b521da7d57a4b45d6cc56
Dmitry Ishkov <dimanrussian@gmail.com>
2012-11-18 03:11:36 +0400

    Implemented InteractionThread, created new Classes

---
+++ server/InteractionThread.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: TheRusskiy
+ * Date: 18.11.12
+ * Time: 1:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class InteractionThread {
+}
---
+++ server/exceptions/NetworkInteractionIsNotReadyException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: TheRusskiy
+ * Date: 18.11.12
+ * Time: 2:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class NetworkInteractionIsNotReadyException {
+}
